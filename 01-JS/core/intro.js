00dca37994ea49bbb811446a14e6148bc09338d7
mat2718 <matthewterry68.mt@gmail.com>
2021-06-07 16:57:10 -0400

    updates

--- 01-JS/core/intro.js
+++ 01-JS/core/intro.js
@@ -1,1 +1,19 @@
+// print hello world
 console.log("Hello World");
+
+
+// data types
+//  number, boolean, string, object, null, bigint, undefined, function, symbol
+
+/*
+
+*/
+
+// variables use let, const, and var
+const age = 12;
+
+//next we need logic
+//  the value below will be a boolean (i.e true or false)
+const overage = (age >= 13);
+
+console.log(typeof undefined);
