feba4dd7abef9e5f7fc0d0619ebb27cca53ed49f
Jared Burkamper <jaredburkamper@gmail.com>
2021-06-07 16:46:11 -0500

    Day one stuff

--- 01-JS-Drelek/intro.js
+++ 01-JS-Drelek/intro.js
@@ -1,1 +1,15 @@
+// I like to think this is playing in the background: https://youtu.be/Yw6u6YkTgQ4
 console.log("Hello, World!");
+
+// const: constant, cannot be updated, block scoped.
+// let: variable, can be updated but not re-declared, block scoped.
+// var: variable, can be updated or re-declared, global scoped.
+
+const age = 12;
+const overAge = age >= 13;
+
+if (overAge) {
+    console.log("Shoo, foolish youngling");
+} else {
+    console.log("Welcome, fellow adult");
+}
