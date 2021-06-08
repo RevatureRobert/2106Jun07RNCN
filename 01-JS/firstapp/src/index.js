c51fcbdb08386fdfa2246bd22b60df2982841c3f
connellrobert <robert.lel.connell@gmail.com>
2021-06-08 15:03:59 -0500

    classes and objects

--- 01-JS/firstapp/src/index.js
+++ 01-JS/firstapp/src/index.js
@@ -80,12 +80,169 @@ function print(something) {
 // const arr5 = new Array(55);
 // console.log(arr5);
 
-arr.map(function (el) {
-    return console.log(el);
-})
-// arrow functions
-arr.map( el => {
-    return console.log(el);
-})
+// arr.map(function (el) {
+//     return console.log(el);
+// })
+//* arrow functions
+// arr.map( el => {
+//     return console.log(el);
+// })
 
-arr.map((el, ind, ar) => console.log(el));
+// arr.map((el, ind, ar) => console.log(el));
+
+
+//* objects
+//      Objects are key value pairs of states and behaviors
+const obj = {
+    name: 'harry',
+    age: 55,
+    sayName: function () {
+        console.log("my name is " + this.name);
+    }
+}
+//* The JSON global object property has methods to convert from json to 
+//*     obj and vice versa.
+// console.log(JSON.stringify(obj));
+const obj2 = JSON.parse(`{
+    "version":"1.03",
+    "pizza":{
+        "toppings":[
+            "pepperoni",
+            "cheese"
+        ],
+        "extraCheese":true,
+        "stuffedCrust":true,
+        "price":10.99
+    },
+    "null":null,
+    "undefined":null
+}`);
+// console.log(-obj2.pizza.price);
+// obj.sayName()
+
+
+// const obj3 = new Object();
+// console.log(obj3.toString());
+
+// console.log(obj2.toString());
+
+// console.log(JSON.stringify(obj.__proto__))
+
+
+//! This is how prototype inheritance is working, remember the objects
+//!     pointing to each other as references for their skeletons
+// const obj7 = Object.create(obj2);
+// console.log(JSON.stringify(obj7.__proto__));
+// obj7.pizza = {};
+// obj7.pizza.toppings = 'something';
+// console.log(obj2.pizza.toppings);
+
+// obj7.version = 2;
+// console.log(obj7.__proto__.version);
+
+// obj7.__proto__.version=17;
+// console.log(obj2.version);
+
+
+
+class Animal {
+    constructor(name){
+        this.name=name;
+    }
+    _name = 'something else';
+}
+
+class Dog extends Animal {
+    constructor(name, color){
+        super(name);
+        this.color=color;
+    }
+    sayName() {
+        console.log(this._name);
+    }
+}
+
+const dog = new Dog('fluffs mguffs','red');
+console.log(dog.name);
+dog.sayName()
+
+//* constructor function, what the class syntax is wrapping
+function Dog2(name, color) {
+    this.name=name;
+    this.color=color;
+    this.sayName = function (){
+        console.log(this.name);
+    }
+}
+
+const dog2 = new Dog2('name', 'color');
+// dog2.sayName()
+
+
+
+//* encapsulation prior to es6
+// closure
+// anonymous function
+// const some = function (){
+//     //something here
+// }
+
+// some()
+
+// IIFE
+//  Immediately Invokable Function Expressions
+// (function () {
+//     console.log('anonymous');
+// })()
+
+
+//* Closure is the ability to nest and return functions that have access to their parents scope
+const driveCar = (function (){
+    console.log("car purchased");
+    let milesDriven = 0;
+    return function (miles){
+        if(miles >0){
+            milesDriven += miles;
+        } 
+        return milesDriven;
+    }
+})()
+
+
+class Sample {
+    constructor(name){
+        this._name=name;
+    }
+    name = (function (n){
+        let _privName = n;
+        return [
+            function (){
+                return _privName;
+            },
+            function (val){
+                _privName = val;
+            }
+        ]
+    })(this._name);
+}
+
+const sample = new Sample('something special')
+
+class GetAndSet {
+    constructor(name){
+        this.#name = name;
+    }
+    #name;
+    get name() {
+        return this._name;
+    }
+
+    set name(val){
+        this._name=val;
+    }
+}
+
+const gas = new GetAndSet('something');
+gas.name = 'hey'
+console.log(gas.name);
+console.log(gas.#name);
