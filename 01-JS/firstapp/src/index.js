4a5af7ad4ce0874c1bf70bf2269e57dea3289410
connellrobert <robert.lel.connell@gmail.com>
2021-06-08 10:52:09 -0500

    arrays

--- 01-JS/firstapp/src/index.js
+++ 01-JS/firstapp/src/index.js
@@ -1,1 +1,34 @@
-console.log('hello npm!!!')
+// arrays
+const arr = [
+  "here is a string",
+  55,
+  undefined,
+  null,
+  {},
+  console,
+  console.log,
+  Symbol(),
+  Error,
+];
+
+// use the square brackets to access the index of the array and assign new 
+//      elements to the array
+console.log(arr[8]);
+arr[3]=17;
+console.log(arr[3]);
+arr[10] = 100
+console.log(arr[100]);
+arr[100]=10
+console.log(arr);
+
+
+const arr2 = []
+arr2[5] = 0
+console.log(arr2);
+
+const l1 = 1;
+// l1++
+const arr3 = [];
+arr3[4]=7;
+arr3[2]=10
+arr3 = [22,34,5];
