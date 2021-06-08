4dbb12c9cbfc84988d6d65d2e7180500c8c35ebb
Louis Casillas <Louis.Casillas@gmail.com>
2021-06-08 16:53:26 -0700

    Fixing variable naming error

--- Assignment01/src/index.cjs
+++ Assignment01/src/index.cjs
@@ -4,8 +4,8 @@ const input_functions = require('../export/get_input.cjs');
 const current_user = new User();
 
 current_user.user_name = input_functions.getInfo('What is your name? ');
-current_user.user_name = input_functions.getInfo('What is your address? ');
-current_user.user_name = input_functions.getInfo('What is your phone number? ');
+current_user.user_address = input_functions.getInfo('What is your address? ');
+current_user.user_phone = input_functions.getInfo('What is your phone number? ');
 
 while ( input_functions.getContinue() )
 {
