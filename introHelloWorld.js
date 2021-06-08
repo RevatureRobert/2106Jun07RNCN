3ef4c4b33cb7f40bbba379eaf4859ccbccb404c6
Thomas-Marik <59934769+Thomas-Marik@users.noreply.github.com>
2021-06-07 21:27:14 -0400

    day one first tries

---
+++ introHelloWorld.js
@@ -0,0 +1,33 @@
+
+   
+   console.log("Hello World");
+
+//data types
+
+
+
+//control flow
+/*
+
+control flow is a way to programatically control teh execution flow of our programs
+
+someone is trying to sign up to our site, must be 13 or over
+
+first , hold teh age in variable
+we can create variable woth three keywords
+
+let const and var
+*/
+const age = 12;
+
+//next we need to write logic for age
+// value below is a boolean
+const overAge =  (age >= 13);
+
+if(overAge){
+    console.log('The person is of age to join site');
+}
+else{
+    console.log('not of age');
+}
+
