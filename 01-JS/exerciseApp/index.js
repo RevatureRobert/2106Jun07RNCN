b0229935743f527e882e410fcb610cb8f0beb4e5
Tyler C. Yates <tyates907@gmail.com>
2021-06-09 08:49:14 -0700

    allow data viewing

--- 01-JS/exerciseApp/index.js
+++ 01-JS/exerciseApp/index.js
@@ -1,21 +1,18 @@
 const User = require('./User.js');
 const prompt = require('prompt-sync')({sigint: true});
 
-let exit = 1;
+let exit = '1';
 while (exit != 0 )
-    if (exit === '0') {
-        console.log('exit program');
-    } else if (exit === 1) {
-        console.log('Enter Data');
-        User.name = prompt('Enter Name: ' )
-        User.address = prompt('Enter Address: ')
-        User.phone = prompt('Enter phone: ')
+    if (exit === '1') {
+        console.log('Enter Data Below');
+        User.name = prompt('Enter Name: ' );
+        User.address = prompt('Enter Address: ');
+        User.phone = prompt('Enter Phone: ');
         exit = 3;
     } else if (exit === '2') {
-        console.log('view data');
+        console.log(`\nUser Data\n-----\nName: ${User.name}\nAddress: ${User.address}\nPhone: ${User.phone}`);
+        exit = 3;
     } else {
-        console.log('(1) Enter Data\n(2) View Data\n(0) Exit Program\n');
-        exit = prompt('Enter Your choice: ')
-        console.log('value is', exit);
-        console.log(typeof exit);
+        console.log('\n(1) Enter Data\n(2) View Data\n(0) Exit Program\n');
+        exit = prompt('Enter Your choice: ');
     }
