819ac804e4b99f3f40de3710dc310ac5418246ff
J19kula <85501862+J19kula@users.noreply.github.com>
2021-06-09 11:49:24 -0400

    Add files via upload

---
+++ exercise/person.js
@@ -0,0 +1,14 @@
+/**
+ * Jacob Kula, June 8th 
+ * This a small application that is used to take in a persons inputs and save them for the person to view later
+ * 
+ * */
+
+//Creation of the Person object
+const person = {
+    "Name":" ",
+    "Address":" ",
+    "Number":" "
+};
+// exporting the person 
+module.exports = person;
