2ea67375b100387ff5108b2f60bcadc5612f4008
Tyler C. Yates <tyates907@gmail.com>
2021-06-09 08:31:29 -0700

    create basic menu and data input

--- 01-JS/exerciseApp/index.js
+++ 01-JS/exerciseApp/index.js
@@ -1,2 +1,21 @@
-const User = require('./User.js')
-const prompt = require('prompt-sync')()
+const User = require('./User.js');
+const prompt = require('prompt-sync')({sigint: true});
+
+let exit = 1;
+while (exit != 0 )
+    if (exit === '0') {
+        console.log('exit program');
+    } else if (exit === 1) {
+        console.log('Enter Data');
+        User.name = prompt('Enter Name: ' )
+        User.address = prompt('Enter Address: ')
+        User.phone = prompt('Enter phone: ')
+        exit = 3;
+    } else if (exit === '2') {
+        console.log('view data');
+    } else {
+        console.log('(1) Enter Data\n(2) View Data\n(0) Exit Program\n');
+        exit = prompt('Enter Your choice: ')
+        console.log('value is', exit);
+        console.log(typeof exit);
+    }
