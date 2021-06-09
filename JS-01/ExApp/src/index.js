a9d9ed30be8080a36b0024b389b8fdbf429adb74
Joab Smith <Joabsmith4226@gmail.com>
2021-06-09 11:21:10 -0400

    Small node module project finished

--- JS-01/ExApp/src/index.js
+++ JS-01/ExApp/src/index.js
@@ -1,6 +1,7 @@
 const Person = require('./personalInfo.js'); //import person class
 
 
+
 const rLS = require('readline-sync'); //import console io module
 let user; //create user
 console.log("Please enter your informaiton:"); 
@@ -14,13 +15,13 @@ checkSelection();
 function checkSelection(){
     let choice = 10;
     console.log();
-    console.log("Available options:");
-    console.log("1: Update Name"); //change name
-    console.log("2: Update address"); //chane address
-    console.log("3: Update phone"); //change phone
-    console.log("4: Add additional information"); //add information
-    console.log("5: Display all current Info")
-    console.log("0: Exit");
+    console.log(`Available options: 
+        1: Update Name
+        2: Update address 
+        3: Update phone 
+        4: Add additional information 
+        5: Display all current Info
+        0: Exit`);
     choice = rLS.question("Please enter your selection: ");
     // switch between chioce of action
     switch(choice){
@@ -50,11 +51,13 @@ function checkSelection(){
             
         };
         case '5': {
-            displayAll();
+            user.displayAll();
             checkSelection();
             break;
         }
         case '0': {
+            console.log(`Final Data`); 
+            user.displayAll();
             console.log("Thanks for useing the app!");
             break;
         };
@@ -65,12 +68,5 @@ function checkSelection(){
     };
 };
 
-function displayAll(){
-    console.log("Name:" + user.name);
-    console.log("Address: " + user.address);
-    console.log("Phone Number: " + user.phone);
-    for(let x of user.moreInfo){
-        console.log(x);
-    }
-}
+
 
--- JS-01/ExApp/src/personalInfo.js
+++ JS-01/ExApp/src/personalInfo.js
@@ -42,7 +42,14 @@ class Person {
         this.#moreInfo = newInfo;
     }
 
-
+    displayAll(){
+        console.log("Name:" + this.#name);
+        console.log("Address: " + this.#address);
+        console.log("Phone Number: " + this.#phone);
+        for(let x of this.#moreInfo){
+            console.log(x);
+        }
+    }
     
 
 
