cc859d0fb6a052fad139f395e16d916e716af9db
Tyler C. Yates <tyates907@gmail.com>
2021-06-09 08:57:16 -0700

    rename variable

--- 01-JS/exerciseApp/index.js
+++ 01-JS/exerciseApp/index.js
@@ -1,18 +1,18 @@
 const User = require('./User.js');
 const prompt = require('prompt-sync')({sigint: true});
 
-let exit = '1';
-while (exit != 0 )
-    if (exit === '1') {
+let run = '1';
+while (run != 0 )
+    if (run === '1') {
         console.log('Enter Data Below');
         User.name = prompt('Enter Name: ' );
         User.address = prompt('Enter Address: ');
         User.phone = prompt('Enter Phone: ');
-        exit = 3;
-    } else if (exit === '2') {
+        run = 3;
+    } else if (run === '2') {
         console.log(`\nUser Data\n-----\nName: ${User.name}\nAddress: ${User.address}\nPhone: ${User.phone}`);
-        exit = 3;
+        run = 3;
     } else {
         console.log('\n(1) Enter Data\n(2) View Data\n(0) Exit Program\n');
-        exit = prompt('Enter Your choice: ');
+        run = prompt('Enter Your choice: ');
     }
