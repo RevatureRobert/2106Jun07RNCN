5c7c0c892f577103a1ef570dd7d1d7ec55c5dd79
jaemowalker <masterjaemo@gmail.com>
2021-06-09 10:53:07 -0400

    Updated npm start working

---
+++ 01-JS/exerciseApp/NewApp/app.js
@@ -0,0 +1,69 @@
+//setup input prompt
+const prompt = require('prompt-sync')({sigint: true});
+
+// prompt and receive input from user
+let name = prompt('What is your name?');
+let address = prompt('What is your address?');
+let phoneNumber = prompt('What is your phone number?');
+let otherInfo = prompt('Would you like to add any other info?')
+
+// create person object from inputs
+let person = {
+    name: (name),
+    address: (address),
+    phoneNumber: (phoneNumber),
+    otherInfo: (otherInfo)
+};
+
+// Display info back to user
+console.log('Thanks, your info is saved, please verify below')
+console.log(person);
+
+// Ask user if they want to update or change anything
+let userUpdate = prompt('Do you wish to change or update anything? Y or N ');
+
+if (userUpdate == 'Y') {
+    console.log('1.Name')
+    console.log('2.Address')
+    console.log('3.Phone Number')
+    console.log('4.Other Info')
+    let newInfo = prompt('Enter the number of the element you would you like to change?');
+    
+// Switch statement to determine what user data to change
+switch (newInfo) {
+  case '1':
+    let name = prompt('What is your name?');
+    console.log('Thanks, your info is saved, please verify below')
+    console.log(name);
+    break;
+
+  case '2':
+    let address = prompt('What is your address?');
+    console.log('Thanks, your info is saved, please verify below')
+    console.log(address);
+    break;
+
+  case '3':
+    let phoneNumber = prompt('What is your phone number?');
+    console.log('Thanks, your info is saved, please verify below')
+    console.log(phoneNumber);
+    break;
+
+  case '4':
+    let otherInfo = prompt('Would you like to add any other info?')
+    console.log('Thanks, your info is saved, please verify below')
+    console.log(otherInfo);
+    break;
+
+  default:
+    console.log(`Sorry, please select from options 1 - 4.`);
+}
+    } else {console.log('Thank you');
+
+    }
+
+
+
+
+   
+    
