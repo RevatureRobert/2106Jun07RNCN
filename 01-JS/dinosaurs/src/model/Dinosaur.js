238aba25a4d3a4af02e10f41cad02e0345ec1bf6
connellrobert <robert.lel.connell@gmail.com>
2021-06-10 12:08:45 -0500

    adding dino project

---
+++ 01-JS/dinosaurs/src/model/Dinosaur.js
@@ -0,0 +1,18 @@
+export class Dinosaur {
+    constructor(name, diet, breed, era, weight, habitat){
+        this.name = name;
+        this.diet = diet;
+        this.breed = breed;
+        this.era = era;
+        this.weight = weight;
+        this.habitat = habitat;
+    }
+    name;
+    diet;
+    breed;
+    era;
+    weight;
+    habitat;
+
+    
+}
