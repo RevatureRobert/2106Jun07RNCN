c1170ade3008ab1ed39e42b5cab8112826cf7c61
Marc Skwarczynski <marc.skwarczynski@revature.net>
2021-06-14 11:57:58 -0700

    Test Commit for Demonstration

---
+++ 01-JS/dinosaurs/app/src/model/dinosaur.test.ts
@@ -0,0 +1,6 @@
+import { Dinosaur } from "./Dinosaur";
+
+test('A new Dinosaur can be created with the new keyword', () => {
+  const dino = new Dinosaur('dinosawr', 'herbivore', 'stegosaurs', 'paleo', 234, forest);
+  expect(dino).toBeTruthy();
+});
--- 01-JS/dinosaurs/app/src/model/dinosaur.ts
+++ 01-JS/dinosaurs/app/src/model/dinosaur.ts
@@ -6,4 +6,8 @@ export class Dinosaur{
     public era,
     public weight,
     public habitat){}
+
+  get nameAndEra(){
+    return `The name is ${this.name} and the era is ${this.era}`;
+  }
 }
