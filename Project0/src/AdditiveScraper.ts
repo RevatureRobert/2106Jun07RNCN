87d693ff345bf2bcd552b2725f6bcf2e706fd21e
unknown <mhassantownsend@gmail.com>
2021-06-13 20:48:06 -0400

    Pushing project_0

---
+++ Project0/src/AdditiveScraper.ts
@@ -0,0 +1,20 @@
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://vx-e-additives.p.rapidapi.com/categories/6");
+
+req.query({
+	"locale": "en"
+});
+
+req.headers({
+	"x-rapidapi-key": "be2957b1f3msh586aed459ef47ffp109e49jsn5f8093a8bbe4",
+	"x-rapidapi-host": "vx-e-additives.p.rapidapi.com",
+	"useQueryString": true
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
