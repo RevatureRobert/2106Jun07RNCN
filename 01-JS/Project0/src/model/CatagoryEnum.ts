86ba6bf475dd1c939845f0f3043d98d736eea4fc
Samuel Smetzer <kamuela.1300@gmail.com>
2021-06-14 14:49:34 -0700

    Project 0 outline

---
+++ 01-JS/Project0/src/model/CatagoryEnum.ts
@@ -0,0 +1,1 @@
+export enum Catagory {DARK, COMSCI, FEELGOOD, MISC}
---
+++ 01-JS/Project0/src/model/Memeobj.ts
@@ -0,0 +1,7 @@
+import { Catagory } from "./CatagoryEnum";
+export class Meme {
+    constructor(
+      public name:string,
+      public catagory:Catagory      
+    ) {}
+  }
