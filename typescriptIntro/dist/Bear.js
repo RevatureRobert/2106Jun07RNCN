f3a3da9c3c93fd3bd7ba5215477b2cea383624c4
imranilyas <68575807+imranilyas@users.noreply.github.com>
2021-06-15 07:15:15 -0700

    Removed old typescript commit

--- typescriptIntro/dist/Bear.js
+++
@@ -1,9 +0,0 @@
-const bear = {
-    fur: 'brown',
-    claws: true,
-    teeth: 47,
-    getScrappy: function () {
-        console.error(`ROAR, I have ${this.teeth} teeth`);
-    }
-};
-bear.getScrappy();
