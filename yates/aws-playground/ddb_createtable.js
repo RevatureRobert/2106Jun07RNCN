255d6262d1c9747f612b673a52e7d85b7326048c
Tyler C. Yates <tyates907@gmail.com>
2021-06-16 21:12:27 -0700

    example create table dynamodb

---
+++ yates/aws-playground/ddb_createtable.js
@@ -0,0 +1,46 @@
+// Import required AWS SDK clients and commands for Node.js
+import { CreateTableCommand } from "@aws-sdk/client-dynamodb";
+import { ddbClient } from "./libs/ddbClient.js";
+
+// Set the parameters
+const params = {
+    AttributeDefinitions: [
+        {
+            AttributeName: "Season", //ATTRIBUTE_NAME_1
+            AttributeType: "N", //ATTRIBUTE_TYPE
+        },
+        {
+            AttributeName: "Episode", //ATTRIBUTE_NAME_2
+            AttributeType: "N", //ATTRIBUTE_TYPE
+        },
+    ],
+    KeySchema: [
+        {
+            AttributeName: "Season", //ATTRIBUTE_NAME_1
+            KeyType: "HASH",
+        },
+        {
+            AttributeName: "Episode", //ATTRIBUTE_NAME_2
+            KeyType: "RANGE",
+        },
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1,
+    },
+    TableName: "TEST_TABLE", //TABLE_NAME
+    StreamSpecification: {
+        StreamEnabled: false,
+    },
+};
+
+const run = async () => {
+    try {
+        const data = await ddbClient.send(new CreateTableCommand(params));
+        console.log("Table Created", data);
+        return data;
+    } catch (err) {
+        console.log("Error", err);
+    }
+};
+run();
