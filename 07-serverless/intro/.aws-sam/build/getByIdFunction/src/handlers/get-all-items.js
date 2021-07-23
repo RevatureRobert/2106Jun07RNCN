203646163a9839b2d9a4fb4ab924b421a6f2811d
connellrobert <robert.lel.connell@gmail.com>
2021-07-23 11:23:41 -0500

    adding serverless tools

---
+++ 07-serverless/intro/.aws-sam/build/getByIdFunction/src/handlers/get-all-items.js
@@ -0,0 +1,24 @@
+const tableName = process.env.SAMPLE_TABLE;
+
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
+
+exports.getAllItemsHandler = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
+    }
+    console.info('received:', event);
+    var params = {
+        TableName : tableName
+    };
+    const data = await docClient.scan(params).promise();
+    const items = data.Items;
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(items)
+    };
+
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+}
