a79e0498b68756be29c0e513f9ad0becfad33836
connellrobert <robert.lel.connell@gmail.com>
2021-07-27 13:22:18 -0500

    adding more sql

--- 08-SQL/intro.sql
+++ 08-SQL/intro.sql
@@ -87,4 +87,85 @@ select * from topics;
 -- an example of how postgres extends the sql standard. This syntax is not supported by all sql engines.
 insert into colors (id, name) values (1, 'blue'), (2, 'grey');
 
+-- different types of multiplicity in sql
+--		1-1, 1-m, m-1, m-m
+--			m-1 or 1-m, foreign key goes inside the many table
+--			1-1, foreign key goes in either table, but must be unique
+--			m-m, needs a junction table (table with a composite key where both primary keys are foreign keys to other tables.
+
+
+
+-- Often in multiplicity, since the relationship is meant to represent aggregation, we want to replace the numerical
+--		representation of the other table with the information itself.
+--	We are able to accomplish this with joins.
+
+
+insert into topics values (2, 'name2', true, 'justified', 1), (3, 'name3',false,'awesome', 2), (4, 'name4', false, 'justified', null);
+insert into colors values ('purple',3);
+
+-- different kinds of joins: inner join
+select topic_name, cool, category, name as label from topics t inner join colors c on topics.label = colors.id;
+
+select topic_name, cool, category, name as label from topics t inner join colors c using (id);
+
+-- left outer join
+
+select topic_name, cool, category, name as label from topics left join colors on topics.label = colors.id;
+
+-- right outer join
+
+select topic_name, cool, category, name as label from topics right join colors on topics.label = colors.id;
+
+-- full outer join
+
+select topic_name, cool, category, name as label from topics full outer join colors on topics.label = colors.id;
+
+-- cross join
+
+select topic_name, cool, category, name as label from topics cross join colors where cool = true;
+
+
+select topic_name, cool, category, name as label from topics t inner join colors c on t.label = c.id;
+
+select topic_name, cool, category, name as label from topics t join colors c on t.label = c.id;
+
+select topic_name, cool, category, name as label from topics t, colors c where t.label = c.id;
+
+
+
+-- set operators
+--		rules for set operators: must be the same number of columns and the same datatypes
+
+create table a (
+	id integer,
+	color text,
+	this_date date
+);
+
+create table b (
+	id integer,
+	color_b text
+);
+
+insert into a values (1, 'red','2012-10-17'),(2,'blue','2015-03-04');
+insert into b values (1, 'purple'),(2,'green');
+insert into b values (3, 'blue');
+
+-- union all
+select color from a union all select color_b from b;
+
+-- union distinct
+select color from a union distinct select color_b from b;
+
+select color from a union select color_b from b;
+
+
+-- minus
+select color from a except select color_b from b;
+select color_b from b except select color from a;
+
+-- intersect
+select color from a intersect select color_b from b;
+
+-- joins will join tables side by side, set operators will return two queries on top of one another
 
